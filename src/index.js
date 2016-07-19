685006875b3a56da118d2d5f75259e694fc70d94
Jenny Gebske <jennifer@gebske.de>
2016-07-19 16:34:08 +0200

    update data on hashchange

--- src/index.js
+++ src/index.js
@@ -13,31 +13,23 @@ function updateHtml(data) {
   document.body.innerHTML = template(data);
 }
 
-var query = queryString.parse(window.location.search);
+function getData() {
+  var query = queryString.parse(window.location.hash);
 
-utils.httpGetJSON(apiUrl + '/documents/remote?' + queryString.stringify({type: query.type, id: query.id}), function(documentErr, documentResponse) {
-  if (documentErr) {
-    console.error(documentErr);
-    return;
-  };
-  if (documentResponse.status === 404) {
-    console.log('Document with type ' + query.type + ' and id ' + query.id + ' not found on PaperHive');
-    return;
-  }
-  if (documentResponse.status !== 200) {
-    console.error('Expected status code 200 (got ' + documentResponse.status + ')');
-    return;
-  }
-  utils.httpGetJSON(apiUrl + '/documents/' +  documentResponse.body.id + '/discussions', function(discussionsErr, discussionsResponse) {
+  utils.httpGetJSON(apiUrl + '/documents/remote?' + queryString.stringify({type: query.type, id: query.id}), function(documentErr, documentResponse) {
     if (documentErr) {
       console.error(documentErr);
       return;
     };
+    if (documentResponse.status === 404) {
+      console.log('Document with type ' + query.type + ' and id ' + query.id + ' not found on PaperHive');
+      return;
+    }
     if (documentResponse.status !== 200) {
       console.error('Expected status code 200 (got ' + documentResponse.status + ')');
       return;
     }
-    utils.httpGetJSON(apiUrl + '/documents/' +  documentResponse.body.id + '/hivers', function(hiversErr, hiversResponse) {
+    utils.httpGetJSON(apiUrl + '/documents/' +  documentResponse.body.id + '/discussions', function(discussionsErr, discussionsResponse) {
       if (documentErr) {
         console.error(documentErr);
         return;
@@ -46,16 +38,28 @@ utils.httpGetJSON(apiUrl + '/documents/remote?' + queryString.stringify({type: q
         console.error('Expected status code 200 (got ' + documentResponse.status + ')');
         return;
       }
-      updateHtml({
-        logo: logo,
-        numDiscussions: discussionsResponse.body.discussions.length,
-        numHives: hiversResponse.body.hivers.length,
-        url: 'https://paperhive.org/documents/' + documentResponse.body.id,
+      utils.httpGetJSON(apiUrl + '/documents/' +  documentResponse.body.id + '/hivers', function(hiversErr, hiversResponse) {
+        if (documentErr) {
+          console.error(documentErr);
+          return;
+        };
+        if (documentResponse.status !== 200) {
+          console.error('Expected status code 200 (got ' + documentResponse.status + ')');
+          return;
+        }
+        updateHtml({
+          logo: logo,
+          numDiscussions: discussionsResponse.body.discussions.length,
+          numHives: hiversResponse.body.hivers.length,
+          url: 'https://paperhive.org/documents/' + documentResponse.body.id,
+        });
       });
     });
   });
-});
+}
 
+window.addEventListener('hashchange', getData);
+getData();
 
 
 // -----
