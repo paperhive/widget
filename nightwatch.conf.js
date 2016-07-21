9000d779771edd0c6515eff9c9eca15d1ae6a25f
Jenny Gebske <jennifer@gebske.de>
2016-07-21 18:21:51 +0200

    first shot at saucelabs config

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,18 +1,6 @@
-module.exports = {
+const lodash = require('lodash');
+const config = {
   src_folders: ['test/e2e/'],
-  selenium: {
-    start_process: true,
-    server_path: './node_modules/selenium-standalone/.selenium/selenium-server/2.53.1-server.jar',
-    /*
-    log_path: '',
-    host: "127.0.0.1",
-    port: 4444,
-    */
-    cli_args: {
-      'webdriver.chrome.driver':
-        './node_modules/selenium-standalone/.selenium/chromedriver/2.22-x64-chromedriver',
-    },
-  },
   test_settings: {
     default: {
       launch_url: 'http://localhost:8080',
@@ -35,3 +23,24 @@ module.exports = {
     },
   },
 };
+
+if (process.env.TRAVIS_JOB_NUMBER) {
+  lodash.assign(config.test_settings, {
+    launch_url: 'http://ondemand.saucelabs.com:80',
+    selenium_port: 80,
+    selenium_host: 'ondemand.saucelabs.com',
+    username: process.env.SAUCE_USERNAME,
+    access_key: process.env.SAUCE_ACCESS_KEY,
+  });
+} else {
+  config.selenium = {
+    start_process: true,
+    server_path: './node_modules/selenium-standalone/.selenium/selenium-server/2.53.1-server.jar',
+    cli_args: {
+      'webdriver.chrome.driver':
+        './node_modules/selenium-standalone/.selenium/chromedriver/2.22-x64-chromedriver',
+    },
+  };
+}
+
+module.exports = config;
