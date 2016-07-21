7dd75d1439f292e0dadd9a4dec81011a4206299a
Jenny Gebske <jennifer@gebske.de>
2016-07-21 15:40:18 +0200

    add babel-runtime

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,8 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
-          presets: ['es2015']
+          presets: ['es2015'],
+          plugins: ['transform-runtime'],
         }
       },
       {
