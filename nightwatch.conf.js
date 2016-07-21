49cb3a331fd15ad859a2dcb36f97adbf403dcec7
Jenny Gebske <jennifer@gebske.de>
2016-07-21 18:27:22 +0200

    add saucelabs capabilities

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -6,31 +6,22 @@ const config = {
       launch_url: 'http://localhost:8080',
       filter: './test/e2e/*.spec.js',
       output_folder: false,
-      /*
-      selenium_port: 4444,
-      selenium_host: 'localhost',
-      silent: true,
-      screenshots: {
-        enabled: false,
-        path: ''
-      },
-      */
-      desiredCapabilities: {
-        browserName: 'chrome',
-        javascriptEnabled: true,
-        acceptSslCerts: true,
-      },
     },
   },
 };
 
 if (process.env.TRAVIS_JOB_NUMBER) {
-  lodash.assign(config.test_settings, {
+  lodash.assign(config.test_settings.default, {
     launch_url: 'http://ondemand.saucelabs.com:80',
     selenium_port: 80,
     selenium_host: 'ondemand.saucelabs.com',
     username: process.env.SAUCE_USERNAME,
     access_key: process.env.SAUCE_ACCESS_KEY,
+    desiredCapabilities: {
+      browserName: 'chrome',
+      build: `build-${process.env.TRAVIS_JOB_NUMBER}`,
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    },
   });
 } else {
   config.selenium = {
@@ -40,6 +31,11 @@ if (process.env.TRAVIS_JOB_NUMBER) {
       'webdriver.chrome.driver':
         './node_modules/selenium-standalone/.selenium/chromedriver/2.22-x64-chromedriver',
     },
+  };
+  config.test_settings.default.desiredCapabilities = {
+    browserName: 'chrome',
+    javascriptEnabled: true,
+    acceptSslCerts: true,
   };
 }
 
