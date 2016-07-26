2294367bcc651a827762a4d2fd1303c62cad33a6
Jenny Gebske <jennifer@gebske.de>
2016-07-26 17:27:28 +0200

    add id and adjust tests

--- test/e2e/widget.spec.js
+++ test/e2e/widget.spec.js
@@ -59,23 +59,22 @@ module.exports = {
   'doi (of script resp. div) exists': browser => {
     browser
       .url(`${browser.launch_url}/index.script.html`)
-      .waitForElementVisible('.ph-widget', 2000)
-      .assert.title('PaperHive widget script')
-      .assert.elementPresent('img')
-      .assert.elementPresent('.ph-badge')
-      .assert.elementPresent('h1')
-      .assert.containsText('h1 > a', 'Read and discuss on PaperHive');
-    browser.expect.element('.ph-description > small').text.to.match(/\d+ discussions?/);
-    browser.expect.element('.ph-description > small').text.to.match(/\d+ hives?/);
+      .waitForElementVisible('#validDoi', 2000)
+      .assert.elementPresent('#validDoi img')
+      .assert.elementPresent('#validDoi .ph-badge')
+      .assert.elementPresent('#validDoi h1')
+      .assert.containsText('#validDoi h1 > a', 'Read and discuss on PaperHive');
+    browser.expect.element('#validDoi .ph-description > small').text.to.match(/\d+ discussions?/);
+    browser.expect.element('#validDoi .ph-description > small').text.to.match(/\d+ hives?/);
     browser.end();
   },
   'doi (of script resp. div) does not exist': browser => {
     browser
       .url(`${browser.launch_url}/index.script.html`)
-      .waitForElementPresent('.paperhive-widget-no-id', 2000)
-      .assert.elementNotPresent('img')
-      .assert.elementNotPresent('.ph-badge')
-      .assert.elementNotPresent('h1')
+      .waitForElementPresent('#invalidDoi', 2000)
+      .assert.elementNotPresent('#invalidDoi img')
+      .assert.elementNotPresent('#invalidDoi .ph-badge')
+      .assert.elementNotPresent('#invalidDoi h1')
       .end();
   },
 };
