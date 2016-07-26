3e4cec2055d0b4d3fe7cba6d82964c6f0d7a4bae
Jenny Gebske <jennifer@gebske.de>
2016-07-26 16:23:51 +0200

    add tests

--- test/e2e/widget.spec.js
+++ test/e2e/widget.spec.js
@@ -1,18 +1,47 @@
 'use strict';
 const utils = require('./utils');
 
+// module.exports = {
+//   before: done => utils.startServer().then(done),
+//   after: done => utils.stopServer().then(done),
+//   // the name is overwritten if it is specified in nightwatch.conf.js
+//   desiredCapabilities: {
+//     name: 'PaperHive widget',
+//   },
+//   'doi exists': browser => {
+//     browser
+//       .url(`${browser.launch_url}#type=doi&id=10.1016/j.neurobiolaging.2016.04.004`)
+//       .waitForElementVisible('.ph-widget', 2000)
+//       .assert.title('PaperHive widget')
+//       .assert.elementPresent('img')
+//       .assert.elementPresent('.ph-badge')
+//       .assert.elementPresent('h1')
+//       .assert.containsText('h1 > a', 'Read and discuss on PaperHive');
+//     browser.expect.element('.ph-description > small').text.to.match(/\d+ discussions?/);
+//     browser.expect.element('.ph-description > small').text.to.match(/\d+ hives?/);
+//     browser.end();
+//   },
+//   'doi does not exist': browser => {
+//     browser
+//       .url(`${browser.launch_url}#type=doi&id=doesntexist`)
+//       .waitForElementNotPresent('.ph-widget', 1000)
+//       .end();
+//   },
+// };
+
+
 module.exports = {
   before: done => utils.startServer().then(done),
   after: done => utils.stopServer().then(done),
   // the name is overwritten if it is specified in nightwatch.conf.js
   desiredCapabilities: {
-    name: 'PaperHive widget',
+    name: 'PaperHive widget script',
   },
   'doi exists': browser => {
     browser
-      .url(`${browser.launch_url}#type=doi&id=10.1016/j.neurobiolaging.2016.04.004`)
+      .url(`${browser.launch_url}/index.script.html`)
       .waitForElementVisible('.ph-widget', 2000)
-      .assert.title('PaperHive widget')
+      .assert.title('PaperHive widget script')
       .assert.elementPresent('img')
       .assert.elementPresent('.ph-badge')
       .assert.elementPresent('h1')
@@ -23,8 +52,11 @@ module.exports = {
   },
   'doi does not exist': browser => {
     browser
-      .url(`${browser.launch_url}#type=doi&id=doesntexist`)
-      .waitForElementNotPresent('.ph-widget', 1000)
+      .url(`${browser.launch_url}/index.script.html`)
+      .waitForElementPresent('.paperhive-widget-no-id', 2000)
+      .assert.elementNotPresent('img')
+      .assert.elementNotPresent('.ph-badge')
+      .assert.elementNotPresent('h1')
       .end();
   },
 };
