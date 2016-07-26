4a152261017729adb291efb973d57f52664fb622
Jenny Gebske <jennifer@gebske.de>
2016-07-26 16:47:12 +0200

    use both tests (for iframe and script)

--- test/e2e/widget.spec.js
+++ test/e2e/widget.spec.js
@@ -35,9 +35,28 @@ module.exports = {
   after: done => utils.stopServer().then(done),
   // the name is overwritten if it is specified in nightwatch.conf.js
   desiredCapabilities: {
-    name: 'PaperHive widget script',
+    name: 'PaperHive widget (iframe and script)',
   },
-  'doi exists': browser => {
+  'doi of iframe exists': browser => {
+    browser
+      .url(`${browser.launch_url}#type=doi&id=10.1016/j.neurobiolaging.2016.04.004`)
+      .waitForElementVisible('.ph-widget', 2000)
+      .assert.title('PaperHive widget')
+      .assert.elementPresent('img')
+      .assert.elementPresent('.ph-badge')
+      .assert.elementPresent('h1')
+      .assert.containsText('h1 > a', 'Read and discuss on PaperHive');
+    browser.expect.element('.ph-description > small').text.to.match(/\d+ discussions?/);
+    browser.expect.element('.ph-description > small').text.to.match(/\d+ hives?/);
+    browser.end();
+  },
+  'doi of iframe does not exist': browser => {
+    browser
+    .url(`${browser.launch_url}#type=doi&id=doesnotexist`)
+    .waitForElementNotPresent('.ph-widget', 1000)
+    .end();
+  },
+  'doi (of script resp. div) exists': browser => {
     browser
       .url(`${browser.launch_url}/index.script.html`)
       .waitForElementVisible('.ph-widget', 2000)
@@ -50,7 +69,7 @@ module.exports = {
     browser.expect.element('.ph-description > small').text.to.match(/\d+ hives?/);
     browser.end();
   },
-  'doi does not exist': browser => {
+  'doi (of script resp. div) does not exist': browser => {
     browser
       .url(`${browser.launch_url}/index.script.html`)
       .waitForElementPresent('.paperhive-widget-no-id', 2000)
