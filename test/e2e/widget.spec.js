42ba519d17630c297238cef3c5bf62eee5c1d307
Jenny Gebske <jennifer@gebske.de>
2016-07-27 12:36:34 +0200

    disable code for tests

--- test/e2e/widget.spec.js
+++ test/e2e/widget.spec.js
@@ -8,45 +8,44 @@ module.exports = {
   desiredCapabilities: {
     name: 'PaperHive widget (iframe and script)',
   },
-  'doi of iframe exists': browser => {
-    browser
-      .url(`${browser.launch_url}#type=doi&id=10.1016/j.neurobiolaging.2016.04.004`)
-      .waitForElementVisible('.ph-widget', 2000)
-      .assert.title('PaperHive widget')
-      .assert.elementPresent('img')
-      .assert.elementPresent('.ph-badge')
-      .assert.elementPresent('h1')
-      .assert.containsText('h1 > a', 'Read and discuss on PaperHive');
-    browser.expect.element('.ph-description > small').text.to.match(/\d+ discussions?/);
-    browser.expect.element('.ph-description > small').text.to.match(/\d+ hives?/);
-    browser.end();
-  },
-  'doi of iframe does not exist': browser => {
-    browser
-    .url(`${browser.launch_url}#type=doi&id=doesnotexist`)
-    .waitForElementNotPresent('.ph-widget', 1000)
-    .end();
-  },
-  // 'doi (of script resp. div) exists': browser => {
+  // 'doi of iframe exists': browser => {
   //   browser
-  //     .url(`${browser.launch_url}/index.script.html`)
-  //     .waitForElementVisible('#validDoi', 2000)
-  //     .assert.elementPresent('#validDoi img')
-  //     .assert.elementPresent('#validDoi .ph-badge')
-  //     .assert.elementPresent('#validDoi h1')
-  //     .assert.containsText('#validDoi h1 > a', 'Read and discuss on PaperHive');
-  //   browser.
-        // expect.element('#validDoi .ph-description > small').text.to.match(/\d+ discussions?/);
-  //   browser.expect.element('#validDoi .ph-description > small').text.to.match(/\d+ hives?/);
+  //     .url(`${browser.launch_url}#type=doi&id=10.1016/j.neurobiolaging.2016.04.004`)
+  //     .waitForElementVisible('.ph-widget', 2000)
+  //     .assert.title('PaperHive widget')
+  //     .assert.elementPresent('img')
+  //     .assert.elementPresent('.ph-badge')
+  //     .assert.elementPresent('h1')
+  //     .assert.containsText('h1 > a', 'Read and discuss on PaperHive');
+  //   browser.expect.element('.ph-description > small').text.to.match(/\d+ discussions?/);
+  //   browser.expect.element('.ph-description > small').text.to.match(/\d+ hives?/);
   //   browser.end();
   // },
-  // 'doi (of script resp. div) does not exist': browser => {
+  // 'doi of iframe does not exist': browser => {
   //   browser
-  //     .url(`${browser.launch_url}/index.script.html`)
-  //     .waitForElementPresent('#invalidDoi', 2000)
-  //     .assert.elementNotPresent('#invalidDoi img')
-  //     .assert.elementNotPresent('#invalidDoi .ph-badge')
-  //     .assert.elementNotPresent('#invalidDoi h1')
-  //     .end();
+  //   .url(`${browser.launch_url}#type=doi&id=doesnotexist`)
+  //   .waitForElementNotPresent('.ph-widget', 1000)
+  //   .end();
   // },
+  'doi (of script resp. div) exists': browser => {
+    browser
+      .url(`${browser.launch_url}/index.script.html`)
+      .waitForElementVisible('#validDoi', 2000)
+      .assert.elementPresent('#validDoi img')
+      .assert.elementPresent('#validDoi .ph-badge')
+      .assert.elementPresent('#validDoi h1')
+      .assert.containsText('#validDoi h1 > a', 'Read and discuss on PaperHive');
+    browser.expect.element('#validDoi .ph-description > small').text.to.match(/\d+ discussions?/);
+    browser.expect.element('#validDoi .ph-description > small').text.to.match(/\d+ hives?/);
+    browser.end();
+  },
+  'doi (of script resp. div) does not exist': browser => {
+    browser
+      .url(`${browser.launch_url}/index.script.html`)
+      .waitForElementPresent('#invalidDoi', 2000)
+      .assert.elementNotPresent('#invalidDoi img')
+      .assert.elementNotPresent('#invalidDoi .ph-badge')
+      .assert.elementNotPresent('#invalidDoi h1')
+      .end();
+  },
 };
