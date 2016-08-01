d92c3636bcd449f00db638d40c4f0bb5713d73b1
Jenny Gebske <jennifer@gebske.de>
2016-08-01 18:54:23 +0200

    get shadow dom elements

--- test/e2e/widget.spec.js
+++ test/e2e/widget.spec.js
@@ -8,7 +8,7 @@ module.exports = {
   desiredCapabilities: {
     name: 'PaperHive widget (iframe and script)',
   },
-  'doi of iframe exists': browser => {
+  'iframe (doi exists)': browser => {
     browser
       .url(`${browser.launch_url}#type=doi&id=10.1016/j.neurobiolaging.2016.04.004`)
       .waitForElementVisible('.ph-widget', 5000)
@@ -21,7 +21,7 @@ module.exports = {
     browser.expect.element('.ph-description > small').text.to.match(/\d+ hives?/);
     browser.end();
   },
-  'doi of iframe does not exist': browser => {
+  'iframe (doi does not exist)': browser => {
     browser
     .url(`${browser.launch_url}#type=doi&id=doesnotexist`)
     .waitForElementNotPresent('.ph-widget', 5000)
@@ -29,20 +29,36 @@ module.exports = {
   },
   // nightwatch can not yet test elements inside shadow DOM, see:
   // (https://github.com/nightwatchjs/nightwatch/issues/192)
-  // 'doi (of script resp. div) exists': browser => {
-  //   browser
-  //     .url(`${browser.launch_url}/index.script.html`)
-  //     .waitForElementVisible('#validDoi', 5000)
-  //     .assert.elementPresent('#validDoi img')
-  //     .assert.elementPresent('#validDoi .ph-badge')
-  //     .assert.elementPresent('#validDoi h1')
-  //     .assert.containsText('#validDoi h1 > a', 'Read and discuss on PaperHive');
-  //   browser.
-        //expect.element('#validDoi .ph-description > small').text.to.match(/\d+ discussions?/);
-  //   browser.expect.element('#validDoi .ph-description > small').text.to.match(/\d+ hives?/);
-  //   browser.end();
-  // },
-  // 'doi (of script resp. div) does not exist': browser => {
+  'script (doi exists)': browser => {
+    let shadowDom;
+    browser
+      .url(`${browser.launch_url}/index.script.html`)
+      .waitForElementVisible('#validDoi', 5000)
+      .execute(function() {
+        return document.body.createShadowRoot !== undefined;
+      }, [], result => { shadowDom = result.value; })
+      .perform(function() {
+        if (shadowDom) {
+          browser.execute(function() {
+            var element = document.getElementById('validDoi');
+            return element.shadowRoot.querySelector('.ph-widget').innerHTML;
+          }, [], (result) => {
+            if (result.error) throw new Error(result.error);
+          });
+        } else {
+          browser.assert.elementPresent('#validDoi > iframe');
+        }
+      });
+    browser.end();
+  },
+    //   .assert.elementPresent('#validDoi img')
+    //   .assert.elementPresent('#validDoi .ph-badge')
+    //   .assert.elementPresent('#validDoi h1')
+    //   .assert.containsText('#validDoi h1 > a', 'Read and discuss on PaperHive');
+    // browser.
+    // expect.element('#validDoi .ph-description > small').text.to.match(/\d+ discussions?/);
+    // browser.expect.element('#validDoi .ph-description > small').text.to.match(/\d+ hives?/);
+  // 'script (doi does not exist)': browser => {
   //   browser
   //     .url(`${browser.launch_url}/index.script.html`)
   //     .waitForElementPresent('#invalidDoi', 5000)
